c6e37cbc9f3df9ea122a18e5c2098198fd1238f9
margarthebar <margarburkart@gmail.com>
2014-11-08 19:43:00 -0500

    tests new get, set, add, remove

--- 01SuperArray/Test.java
+++ 01SuperArray/Test.java
@@ -13,24 +13,29 @@ public class Test{
 	a.set(7,'h');
 	a.set(8,'i');
 	a.set(9,'j');
-	System.out.println(a.toString());
+	System.out.println("Original:\n"+a.toString());
 	
-	System.out.println(a.get(5));
+	System.out.println("Testing get(5):\n"+a.get(5));
 
-	a.set(8,"Hello");
-	System.out.println(a.toString());
+	System.out.println(a.set(8,"Hello"));
+	System.out.println("set(8,'Hello'):\n"+a.toString());
 
-	a.add(2);
-	System.out.println(a.toString());
-	a.add(new Integer(99));
-	System.out.println(a.toString());
+	a.add(new Integer(2));
+	System.out.println("add(new Integer(2)):\n"+a.toString());
+
+	a.add(3,new Integer(99));
+	System.out.println("add(3,new Integer(99)):\n"+a.toString());
+
 	a.add(new Character('Z'));
-	System.out.println(a.toString());
+	System.out.println("add(new Character('Z')):\n"+a.toString());
 
+	System.out.println(a.remove(7));
+	System.out.println("a.remove(7):\n"+a.toString());
 
 	a.resize(5);
-	System.out.println(a.toString());
+	System.out.println("a.resize(5):\n"+a.toString());
+
 	a.clear();
-	System.out.println(a.toString());
+	System.out.println("a.clear():\n"+a.toString());
     }
 }
