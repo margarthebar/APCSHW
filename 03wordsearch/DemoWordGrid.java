dc5d959868b5017354439581fab1c49727a05b40
margarthebar <margarburkart@gmail.com>
2014-11-23 14:57:25 -0500

    new reads from file

--- 03wordsearch/DemoWordGrid.java
+++ 03wordsearch/DemoWordGrid.java
@@ -1,50 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
+
 public class DemoWordGrid{
     private static WordGrid puzzle = new WordGrid(5,5);
     private static WordGrid puzzle1 = new WordGrid(5,5);
+    private static ArrayList<String> words = new ArrayList<String>();
 
-    public static void main(String[]args){
-	System.out.println(puzzle.toString());
+    public static void main(String[]args) throws FileNotFoundException, NoSuchElementException{
 
-	System.out.println(puzzle.addWordHorizontal("CAT",3,3));
-	System.out.println(puzzle.toString());
+	File input = new File("words.txt");
+	Scanner in = new Scanner(input);
+	String word = "";
+	while(in.hasNext()){
+	    words.add(in.next());
+	}
 
-	System.out.println(puzzle.addWordHorizontal("BUNNY",3,0));
-	System.out.println(puzzle.toString());
+	System.out.println(words.toString());
 
-	System.out.println(puzzle.addWordVertical("STEAK",0,2));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordVertical("FIEND",1,2));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordVertical("FIEND",1,1));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordVertical("FIEND",0,2));
-	System.out.println(puzzle.toString());
-	
-	System.out.println(puzzle.addWordDiagonal("STEAK",0,0));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordDiagonal("AIRY",0,1));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordDiagonal1("FAR",1,3));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle.addWordDiagonal1("FAR",0,2));
-	System.out.println(puzzle.toString());
-
-	System.out.println(puzzle1.addWordHorizontal(puzzle1.flip("BUNNY"),1,0));
-	System.out.println(puzzle1.toString());
-
-	System.out.println(puzzle1.addWordVertical(puzzle1.flip("FIEND"),0,2));
-	System.out.println(puzzle1.toString());
-
-	System.out.println(puzzle1.addWordDiagonal(puzzle1.flip("AIRY"),1,0));
-	System.out.println(puzzle1.toString());
-
-	System.out.println(puzzle1.addWordDiagonal1(puzzle1.flip("FAR"),2,4));
-	System.out.println(puzzle1.toString());
     }
 }
