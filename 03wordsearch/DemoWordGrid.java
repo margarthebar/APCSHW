740a57a8583d932279b2571690ae509d40addbf6
margarthebar <margarburkart@gmail.com>
2014-11-23 14:08:33 -0500

    tests addWordDiagonal1 and flip

--- 03wordsearch/DemoWordGrid.java
+++ 03wordsearch/DemoWordGrid.java
@@ -1,5 +1,6 @@
 public class DemoWordGrid{
     private static WordGrid puzzle = new WordGrid(5,5);
+    private static WordGrid puzzle1 = new WordGrid(5,5);
 
     public static void main(String[]args){
 	System.out.println(puzzle.toString());
@@ -27,5 +28,23 @@ public class DemoWordGrid{
 
 	System.out.println(puzzle.addWordDiagonal("AIRY",0,1));
 	System.out.println(puzzle.toString());
+
+	System.out.println(puzzle.addWordDiagonal1("FAR",1,3));
+	System.out.println(puzzle.toString());
+
+	System.out.println(puzzle.addWordDiagonal1("FAR",0,2));
+	System.out.println(puzzle.toString());
+
+	System.out.println(puzzle1.addWordHorizontal(puzzle1.flip("BUNNY"),1,0));
+	System.out.println(puzzle1.toString());
+
+	System.out.println(puzzle1.addWordVertical(puzzle1.flip("FIEND"),0,2));
+	System.out.println(puzzle1.toString());
+
+	System.out.println(puzzle1.addWordDiagonal(puzzle1.flip("AIRY"),1,0));
+	System.out.println(puzzle1.toString());
+
+	System.out.println(puzzle1.addWordDiagonal1(puzzle1.flip("FAR"),2,4));
+	System.out.println(puzzle1.toString());
     }
 }
