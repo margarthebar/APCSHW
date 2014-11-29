db589dc063de046830925b23cd0f4acedaa1cceb
margarthebar <margarburkart@gmail.com>
2014-11-29 11:35:38 -0600

    moved logic to WordGrid

--- 03wordsearch/DemoWordGrid.java
+++ 03wordsearch/DemoWordGrid.java
@@ -4,66 +4,29 @@ import java.util.*;
 
 public class DemoWordGrid{
     private static WordGrid puzzle = new WordGrid(14,14);
-    private static ArrayList<String> words = new ArrayList<String>();
-
-    public static String flip(String word){
-	String flipped = "";
-	for(int i=0; i<word.length(); i++){
-	    flipped +=word.charAt(word.length()-1-i);
-	}
-	return flipped;
-    }
+    //   private static ArrayList<String> words = new ArrayList<String>();
 
     public static void main(String[]args) throws FileNotFoundException, NoSuchElementException{
 
 	File input = new File("words.txt");
 	Scanner in = new Scanner(input);
 
-	while(in.hasNext()){
-	    words.add(in.next());
-	}
-
-	System.out.println(words.toString());
-
-	Random rand = new Random();
-	String word = "";
-	boolean done = false;
-	int tries = 0;
-
-	for(int i=0; i<words.size(); i++){
-	    while(!done && tries<100){
-		word = words.get(i);
-
-		if(rand.nextInt(2) == 1){
-			word = flip(word);
-		}
-
-		if(rand.nextInt(4) == 0){
-		    if(puzzle.addWordHorizontal(words.get(i),rand.nextInt(14),rand.nextInt(14))){
-			done = true;
-		    }
-		}else if(rand.nextInt(4) == 1){
-		    if(puzzle.addWordVertical(words.get(i),rand.nextInt(14),rand.nextInt(14))){
-			done = true;
-		    }
-		}else if(rand.nextInt(4) == 2){
-		    if(puzzle.addWordDiagonal(words.get(i),rand.nextInt(14),rand.nextInt(14))){
-			done = true;
-		    }
-		}else{
-		    if(puzzle.addWordDiagonal1(words.get(i),rand.nextInt(14),rand.nextInt(14))){
-			done = true;
-		    }
-		}
-		tries++;
-	    }
-	    done = false;
-	}
-
-        puzzle.fill();
-
+	/*this is the "NEW SECTION" 
+	 *
+	 *This is where you need to write something in order
+	 *to get the values from args, OR decide to use default values 
+	 */
+	
+	//WordGrid w = new WordGrid( /*some stuff from NEW SECTION*/ );
+	//if(you need to change the random seed){  /*see more notes on this later*/
+	//    w.setSeed( ??? );  
+	//}
+	//w.loadWordsFromFile("fileNameYouMade.txt", /*something from NEW SECTION*/ );
+	//System.out.println( "Find these words:\n"+ w.wordsInPuzzle() );
+	//System.out.println( w );
+	
+	puzzle.grid();
 	System.out.println(puzzle.toString());
 
     }
 }
-
