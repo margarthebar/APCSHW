b4ed34c9879595818d0889ea1ce423f034a85b68
margarthebar <margarburkart@gmail.com>
2014-12-03 12:30:26 -0500

    fixed return error

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -8,18 +8,16 @@ public class OrderedSuperArray extends SuperArray2{
     public OrderedSuperArray(){
 	super(10);
     }
-
     public void add(String e){
 	super.add(e);
-	super.insertionSort(data);
+	insertionSort(data);
     }
-
     public void add(int index, String e){
 	this.add(e);
     }
-
     public String set(int index, String e){
-        super.set(index,e);
-	super.insertionSort(data);
+	String ans = super.set(index,e);
+	insertionSort(data);
+	return ans;
     }
 }
