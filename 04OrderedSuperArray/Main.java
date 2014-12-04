7498c22974fcc09ada4662f9d2db3e83f7bf3407
margarthebar <margarburkart@gmail.com>
2014-12-04 12:28:36 -0500

    tests find in SuperArray2 (works)

--- 04OrderedSuperArray/Main.java
+++ 04OrderedSuperArray/Main.java
@@ -1,5 +1,20 @@
 public class Main3{
     public static void main(String[]args){
+    	SuperArray2 b = new SuperArray2();
+	b.add("Mary");
+	b.add("had");
+	b.add("a");
+	b.add("little");
+	b.add("lamb");
+	b.add("chop");
+	b.add("had");
+	b.add("little");
+	System.out.println(b.toString());
+
+	System.out.println(b.find("little"));
+	System.out.println(b.find("lamb"));
+	System.out.println(b.find("kebob"));
+    	
 	OrderedSuperArray a = new OrderedSuperArray();
 	System.out.println(a.toString());
 	a.add("dog");
