cedf19f6bf44d2fa27a9dc26f8ea090e198070b9
margarthebar <margarburkart@gmail.com>
2014-12-05 12:34:47 -0500

    Create TestSorts.java

---
+++ 04OrderedSuperArray/TestSorts.java
@@ -0,0 +1,24 @@
+public class TestSorts{
+    public static void main(String[]args){
+	SuperArray2 a = new SuperArray2();
+	for(int i=0; i<500000; i++){
+	    a.add(""+i);
+	}
+	a.insertionSort();
+	System.out.println(insetionSort done);
+
+	SuperArray2 a = new SuperArray2();
+	for(int i=0; i<500000; i++){
+	    a.add(""+i);
+	}
+	a.insertionSort();
+	System.out.println(insetionSort done);
+
+	SuperArray2 a = new SuperArray2();
+	for(int i=0; i<500000; i++){
+	    a.add(""+i);
+	}
+	a.insertionSort();
+	System.out.println(insetionSort done);
+    }
+}
