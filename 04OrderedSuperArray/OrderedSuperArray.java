4513daa90e216cdec2164cd1a4fe4b1290ef2f3d
margarthebar <margarburkart@gmail.com>
2014-12-05 06:19:06 -0500

    everything works but find

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,9 +1,6 @@
 public class OrderedSuperArray extends SuperArray2{
-    private String[] data;
-    private int capacity;
     public OrderedSuperArray(int size){
-	capacity = size;
-	data = new String[size];
+        super();
     }
     public OrderedSuperArray(){
 	super(10);
@@ -19,5 +16,48 @@ public class OrderedSuperArray extends SuperArray2{
 	String ans = super.set(index,e);
 	insertionSort();
 	return ans;
+    }
+    public int find(String target){
+	int upper = size();
+	int lower = 0;
+	int index = size()/2;
+	int real = index;
+	String[] copy = new String[size()];
+	boolean loop = true;
+
+	while(loop){
+	    if(size()<=1){
+		loop = false;
+		return -1;
+	    }
+	    if(getData()[index].equals(target)){
+		System.out.println("found");
+		for(int i=index-1;i>=0;i--){
+		    if(getData()[i].equals(target)){
+			index = i;
+		    }else{
+			loop = false;
+			return index;
+		    }
+		}   
+	    }else if(getData()[index].compareTo(target)>0){
+		int i=index;
+		while(i<size()){
+		    remove(i);
+		    System.out.println(toString());
+		}
+		System.out.println(size()/2);
+		index = size()/2;
+	    }else{
+		String x = getData()[index];
+		while(!getData()[0].equals(x)){
+		    remove(0);
+		    System.out.println(toString());
+		}
+		System.out.println(size()/2);
+		index = size()/2;
+	    }
+	}
+	return index;
     }
 }
