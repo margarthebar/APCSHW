886dc09c57a73eccc34a40c1923600dafb0b3af9
margarthebar <margarburkart@gmail.com>
2014-12-06 20:59:45 -0500

    Update TestSorts.java

--- 04OrderedSuperArray/TestSorts.java
+++ 04OrderedSuperArray/TestSorts.java
@@ -1,24 +1,30 @@
 public class TestSorts{
     public static void main(String[]args){
 	SuperArray2 a = new SuperArray2();
-	for(int i=0; i<500000; i++){
+	for(int i=0; i<100000; i++){
 	    a.add(""+i);
 	}
+	long start = System.currentTimeMillis();
 	a.insertionSort();
-	System.out.println(insetionSort done);
+	long end = System.currentTimeMillis();
+	System.out.println("insertionSort done: "+(end-start));
 
-	SuperArray2 a = new SuperArray2();
-	for(int i=0; i<500000; i++){
-	    a.add(""+i);
+	SuperArray2 b = new SuperArray2();
+	for(int i=0; i<100000; i++){
+	    b.add(""+i);
 	}
-	a.insertionSort();
-	System.out.println(insetionSort done);
+	start = System.currentTimeMillis();
+	b.insertionSort();
+	end = System.currentTimeMillis();
+	System.out.println("selectionSort done: "+(end-start));
 
-	SuperArray2 a = new SuperArray2();
-	for(int i=0; i<500000; i++){
-	    a.add(""+i);
+	SuperArray2 c = new SuperArray2();
+	for(int i=0; i<100000; i++){
+	    c.add(""+i);
 	}
-	a.insertionSort();
-	System.out.println(insetionSort done);
+	start = System.currentTimeMillis();
+	c.badInsertionSort();
+	end = System.currentTimeMillis();
+	System.out.println("badInsertionSort done: "+(end-start));
     }
 }
