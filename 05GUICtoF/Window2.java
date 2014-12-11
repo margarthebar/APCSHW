f3eb52d9b6cfd02d2c021bf24319d66ea055b4a3
margarthebar <margarburkart@gmail.com>
2014-12-10 22:52:11 -0500

    layout fixed with radio buttons

--- 05GUICtoF/Window2.java
+++ 05GUICtoF/Window2.java
@@ -6,42 +6,56 @@ public class Window2 extends JFrame implements ActionListener{//requires overrid
     private JButton b;
     private JLabel lt,lc;
     private JTextField t;
-    private JCheckBox c,f;
+    private JRadioButton c,f;
+    private ButtonGroup group;
     public Window2() {
 	this.setTitle("Celsius-Fahrenheit Converter");
-	this.setSize(600,200);
+	this.setSize(400,100);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
 	pane = this.getContentPane();
-	pane.setLayout(new GridLayout(3,1));
-	
+	pane.setLayout(new GridLayout(2,3));
+
 	lt = new JLabel("Input: ",null,JLabel.CENTER);
-	t = new JTestField(12);
+	t = new JTextField(12);
 	lc = new JLabel("Convert to:",null,JLabel.CENTER);
-	c = new JCheckBox("Celsius");
-	f = new JCheckBox("Fahrenheit");
+	c = new JRadioButton("Celsius",true);
+	f = new JRadioButton("Fahrenheit",false);
         b = new JButton("Convert!");
+	group = new ButtonGroup();
+	group.add(c);
+	group.add(f);
 
-
-	b.addActionListener(this);
-	b.setActionCommand("Convert!");
-
-	pane.add(lt);
-	pane.add(t);
 	pane.add(lc);
 	pane.add(c);
 	pane.add(f);
+	pane.add(lt);
+	pane.add(t);
 	pane.add(b);
+
+	b.addActionListener(this);
+	b.setActionCommand("Convert!");
+    }
+
+    public long convert(long num){
+	long ans = 0.0;
+	if(c.isSelected()){
+	    ans = (num-32)*(5/9);
+	}else{
+	    ans = (num+32)*(9/5);
+	}
     }
 
     public void actionPerformed(ActionEvent e){
 	String s = e.getActionCommand();       
 	System.out.println(s);
 	if(c.isSelected()){
+	    System.out.println("Celsius selected");
 	    //do conversion to Celsius
 	}else if(f.isSelected()){
+	    System.out.println("Fahrenheit selected");
 	    //do converion to Fahrenheit
 	}
     }
